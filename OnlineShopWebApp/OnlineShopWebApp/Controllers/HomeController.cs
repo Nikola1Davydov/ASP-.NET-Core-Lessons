53c5e9a68b9370ffebc0bc969c3cbff0ec7d34b9
Nikolai Davydov <nik_davydov_1992@outlook.com>
2024-09-05 23:53:29 +0200

    lesson 3 is finished

--- OnlineShopWebApp/OnlineShopWebApp/Controllers/HomeController.cs
+++ OnlineShopWebApp/OnlineShopWebApp/Controllers/HomeController.cs
@@ -1,21 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using OnlineShopWebApp.Models;
 using System.Diagnostics;
+using System.Xml;
 
 namespace OnlineShopWebApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}#
+       
 
-        public IActionResult Index()
+        public string Index()
         {
-            return View();
+            List<Waren> listWaren = new List<Waren>();
+            for (int i = 1; i < 4; i++)
+            {
+                listWaren.Add(new Waren("Id"+i, "Name"+i, "Cost"+i));
+            }
+            var test = listWaren.Select(x => x.Info());
+            return String.Join(Environment.NewLine, test);
         }
 
         public IActionResult Privacy()
---
+++ OnlineShopWebApp/OnlineShopWebApp/Waren.cs
@@ -0,0 +1,21 @@
+﻿namespace OnlineShopWebApp
+{
+    public class Waren
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Cost { get; set; }
+        public string Description { get; set; }
+
+        public Waren(string id, string name, string cost)
+        {
+            Id = id;
+            Name = name;
+            Cost = cost;
+        }
+        public string Info()
+        {
+            return String.Join(Environment.NewLine, [Id, Name, Cost]);
+        }
+    }
+}
