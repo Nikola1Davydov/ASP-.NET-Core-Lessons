fa61f44d7881908c02e5df2295454aeea9dead9b
Nikolai Davydov <nik_davydov_1992@outlook.com>
2024-09-30 22:37:58 +0200

    задание 3 завершено

---
+++ OnlineShopWebApp/OnlineShopWebApp/Controllers/AuthenticationController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+using System.Diagnostics;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public AuthenticationController()
+        {
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(Authentication authentication)
+        {
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RegistrationButton(Authentication authentication)
+        {
+            return RedirectToAction("Registration");
+        }
+    }
+}
---
+++ OnlineShopWebApp/OnlineShopWebApp/Models/Authentication.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineShopWebApp.Models
+{
+    public class Authentication
+    {
+        public string Login { get; set; }
+        public string Passwort { get; set; }
+        public string PasswortRepeat { get; set; }
+        public bool RememberMe { get; set; }
+        public string UserId { get; set; }
+    }
+}
